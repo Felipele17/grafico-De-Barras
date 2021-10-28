0e1ec74188293b0a30e9f58be5efe062393107e8
Felipe Torales Leite <felipetolei@hotmail.com>
2021-10-28 16:46:27 -0300

    Initial Commit

---
+++ grafico-De-Barras/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grafico-De-Barras
+//
+//  Created by Felipe Leite on 28/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ grafico-De-Barras/grafico_De_BarrasApp.swift
@@ -0,0 +1,17 @@
+//
+//  grafico_De_BarrasApp.swift
+//  grafico-De-Barras
+//
+//  Created by Felipe Leite on 28/10/21.
+//
+
+import SwiftUI
+
+@main
+struct grafico_De_BarrasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
